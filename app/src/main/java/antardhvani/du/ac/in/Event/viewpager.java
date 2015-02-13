d1cb776e318e10d5eecfb05e3ed98a553c46e180
Rajan Maurya <rajanmaurya154@gmail.com>
2015-02-13 19:42:35 +0530

    viewpager

--- app/src/main/java/antardhvani/du/ac/in/Event/viewpager.java
+++ app/src/main/java/antardhvani/du/ac/in/Event/viewpager.java
@@ -7,23 +7,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
-import android.widget.TextView;
 
 import antardhvani.du.ac.in.antardhvani.R;
 import it.neokree.materialtabs.MaterialTab;
 import it.neokree.materialtabs.MaterialTabListener;
 
-/**
- * Created by Windows on 23-01-2015.
- */
+
 public class viewpager extends Fragment implements MaterialTabListener {
-    private TextView textView;
-    private Toolbar toolbar;
+
     //public static MaterialTabHost tabHost;
     public static TabHost oldTabHost;
     public static ViewPager viewPager;
@@ -44,66 +39,17 @@ public class viewpager extends Fragment implements MaterialTabListener {
         View layout;
 
 
-            layout = inflater.inflate(R.layout.viewpager, container, false);
-            Bundle bundle = getArguments();
-            tabHost = (PagerTabStrip) layout.findViewById(R.id.materialTabHost);
-            viewPager = (ViewPager) layout.findViewById(R.id.viewPager);
-            ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
-            viewPager.setAdapter(adapter);
+        layout = inflater.inflate(R.layout.viewpager, container, false);
+        Bundle bundle = getArguments();
+        tabHost = (PagerTabStrip) layout.findViewById(R.id.materialTabHost);
+        viewPager = (ViewPager) layout.findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
-//            viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//                @Override
-//                public void onPageSelected(int position) {
-//                    tabHost.setSelectedNavigationItem(position);
-//                }
-//            });
-//
-//            for (int i = 0; i < adapter.getCount(); i++) {
-//                tabHost.addTab(
-//                        tabHost.newTab()
-//                                //.setIcon(adapter.getPageTitle(i))
-//                                .setText(adapter.getPageTitle(i))
-//
-//                                .setTabListener(this));
-//
-//
-//            }
-//
-//
-//        }
-//        else {
-//
-//            layout = inflater.inflate(R.layout.viewpager_compat, container, false);
-//            Bundle bundle = getArguments();
-//            oldTabHost = (TabHost) layout.findViewById(R.id.tabHost);
-//            viewPager = (ViewPager) layout.findViewById(R.id.viewPagerCompat);
-//
-//            ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
-//            viewPager.setAdapter(adapter);
-//            viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//                @Override
-//                public void onPageSelected(int position) {
-//                    oldTabHost.setCurrentTab(position);
-//                }
-//            });
-//
-//            for (int i = 0; i < adapter.getCount(); i++) {
-//                oldTabHost.addTab(
-//                        oldTabHost.newTabSpec("Str").setIndicator(adapter.getPageTitle(i)));
-//                                //.setIcon(adapter.getPageTitle(i))
-//                               // .setText(adapter.getPageTitle(i))
-//
-//                                //.setTabListener(this));
-//
-//
-//            }
-//
-//
-//        }
-            return layout;
+        return layout;
 
 
-        }
+    }
 
     @Override
     public void onTabSelected(MaterialTab materialTab) {
@@ -121,75 +67,60 @@ public class viewpager extends Fragment implements MaterialTabListener {
     }
 
 
-//    @Override
-//    public void onTabSelected(MaterialTab materialTab) {
-//        viewPager.setCurrentItem(materialTab.getPosition());
-//    }
-//
-//    @Override
-//    public void onTabReselected(MaterialTab materialTab) {
-//
-//    }
-//
-//    @Override
-//    public void onTabUnselected(MaterialTab materialTab) {
-//
-//    }
-//}
 
-        class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
 
-            public Context context;
+        public Context context;
 
-            public ViewPagerAdapter(FragmentManager fm) {
-                super(fm);
-
-
-            }
-
-
-            public Fragment getItem(int num) {
-                switch (num) {
-                    case 0: // Fragment # 0 - This will show image
-                        //return Event_non_competitive.getInstance(num);
-                        return new Event_non_competitive();
-                    case 1: // Fragment # 1 - This will show image
-                        return new Event_competitive();
-                    //return Event_competitive.getInstance(num);
-                    case 2:// Fragment # 2-9 - Will show list
-                        return new Event_special();
-//            default:// Fragment # 2-9 - Will show list
-//                return Event_non_competitive.getInstance(num);
-
-                }
-                return null;
-                //return Expand.getInstance(num);
-            }
-
-
-            @Override
-            public int getCount() {
-                return 3;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                String title = null;
-                switch (position) {
-                    case 0:
-                        title = "Non Competitive";
-                        break;
-                    case 1:
-                        title = "Competitive";
-                        break;
-                    case 2:
-                        title = "Special Attractions";
-                        break;
-                }
-                return title;
-            }
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
 
 
         }
+
+
+        public Fragment getItem(int num) {
+            switch (num) {
+                case 0: // Fragment # 0 - This will show image
+                    //return Event_non_competitive.getInstance(num);
+                    return new Event_non_competitive();
+                case 1: // Fragment # 1 - This will show image
+                    return new Event_competitive();
+                //return Event_competitive.getInstance(num);
+                case 2:// Fragment # 2-9 - Will show list
+                    return new Event_special();
+//            default:// Fragment # 2-9 - Will show list
+//                return Event_non_competitive.getInstance(num);
+
+            }
+            return null;
+            //return Expand.getInstance(num);
+        }
+
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = null;
+            switch (position) {
+                case 0:
+                    title = "Non Competitive";
+                    break;
+                case 1:
+                    title = "Competitive";
+                    break;
+                case 2:
+                    title = "Special Attractions";
+                    break;
+            }
+            return title;
+        }
+
+
     }
+}
