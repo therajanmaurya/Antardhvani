d5b472d3b16bb62065f8e3d8eb6a817cd77c52f1
Sagar <kumarsagar15795@gmail.com>
2015-02-14 13:17:23 +0530

    Toolbar update

--- app/src/main/java/antardhvani/du/ac/in/antardhvani/MainActivity.java
+++ app/src/main/java/antardhvani/du/ac/in/antardhvani/MainActivity.java
@@ -29,11 +29,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
