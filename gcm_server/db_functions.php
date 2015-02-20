ef2d67335fbe39fd6e68a2bf7b7ba45cf493843f
akki12345 <akoli0082@gmail.com>
2015-02-20 14:30:22 +0530

    Gcm Server Added

---
+++ gcm_server/db_functions.php
@@ -0,0 +1,54 @@
+<?php
+
+class DB_Functions {
+
+    private $db;
+
+    //put your code here
+    // constructor
+    function __construct() {
+        include_once './db_connect.php';
+        // connecting to database
+        $this->db = new DB_Connect();
+        $this->db->connect();
+    }
+
+    // destructor
+    function __destruct() {
+        
+    }
+
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($gcm_regid) {
+        // insert user into database
+        $result = mysql_query("INSERT INTO gcm_users(gcm_regid, created_at) VALUES($gcm_regid', NOW())");
+        // check for successful store
+        if ($result) {
+            // get user details
+            $id = mysql_insert_id(); // last inserted id
+            $result = mysql_query("SELECT * FROM gcm_users WHERE id = $id") or die(mysql_error());
+            // return user details
+            if (mysql_num_rows($result) > 0) {
+                return mysql_fetch_array($result);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Getting all users
+     */
+    public function getAllUsers() {
+        $result = mysql_query("select * FROM gcm_users");
+        return $result;
+    }
+
+
+}
+
+?>
---
+++ gcm_server/register.php
@@ -0,0 +1,30 @@
+<?php
+
+// response json
+$json = array();
+
+/**
+ * Registering a user device
+ * Store reg id in users table
+ */
+if (isset($_POST["regId"])) {
+    $gcm_regid = $_POST["regId"]; // GCM Registration ID
+    // Store user details in db
+    include_once './db_functions.php';
+    include_once './GCM.php';
+
+    $db = new DB_Functions();
+    $gcm = new GCM();
+
+    $res = $db->storeUser($gcm_regid);
+
+    $registatoin_ids = array($gcm_regid);
+    $message = array("product" => "shirt");
+
+    $result = $gcm->send_notification($registatoin_ids, $message);
+
+    echo $result;
+} else {
+    // user details missing
+}
+?>
